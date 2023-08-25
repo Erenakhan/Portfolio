416cd2f00ccda557abb2deff6c350ad2d0819209
Erenakhan <mustafaakhan794@gmail.com>
2023-08-25 17:28:29 +0300

    resume button added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,11 +8,23 @@ export default function Main() {
     const handleFirstTextComplete = () => {
       setShowSecondText(true);
     };
-  
+
+    const pdf_file_url = 'https://erenakhan.vercel.app/cv.pdf';
+
+    function fileDownloader(url) {
+      const fileName = url.split("/").pop();
+      const aTag = document.createElement("a");
+      aTag.href = url;
+      aTag.setAttribute("download", fileName);
+      document.body.appendChild(aTag);
+      aTag.click();
+      document.body.removeChild(aTag); // Sadece 'aTag.remove();' yerine daha güvenli bir kaldırma işlemi yapılıyor.
+    }
+
   return (
     <>
       <div id='home' className='h-screen '>
-        <div className='h-1/4 w-full md:text-3xl text-xl md:gap-5 gap-3 md:pr-3 pr-1 flex justify-end items-center text-white'>
+        <div className='h-1/4 w-full md:text-3xl text-lg md:gap-5 gap-3 md:pr-3 pr-1 flex justify-end items-center text-white'>
           <a href="#about" className='group transition duration-300'>
             About me
             <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-primary"></span>
@@ -41,7 +53,7 @@ export default function Main() {
           )}
           </div>
         </div>
-        <div className='h-1/4 w-full flex justify-center gap-[20px] md:gap-[100px] lg:gap-[150px]'>
+        <div className='h-1/8 w-full flex justify-center gap-[20px] md:gap-[100px] lg:gap-[150px] '>
 
         <a href="https://github.com/Erenakhan">
         <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" width="100" height="100" className='hover:scale-110 md:h-15 md:w-15 h-12 w-12 lg:w-20 lg:h-20' viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path></svg>
@@ -49,11 +61,25 @@ export default function Main() {
 
        <a href="https://www.linkedin.com/in/mustafa-eren-akhan/">
        <svg  xmlns="http://www.w3.org/2000/svg" fill="#ffffff" width="100" height="100"  className='hover:scale-110 md:h-15 md:w-15 h-12 w-12 lg:w-20 lg:h-20' viewBox="0 0 24 24"><path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"></path></svg>
-       </a>
-            
+       </a>     
+        </div>
+       
+        <div className='w-full flex p-16 h-1/8  items-center '>
+        <button className='flex items-center mx-auto gap-2 hover:scale-110' onClick={() => fileDownloader(pdf_file_url)}>
+        <p className='text-white font-semibold md:text-xl text-md '>Download Cv</p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="text-white md:w-12 md:h-12 h-10 w-10">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+        </svg>
+
+        </button>
+       
+        
+
         </div>
         
+        
         <div className='w-full h-1/4 flex justify-center '>
+          
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 animate-bounce text-white">
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
         </svg>
